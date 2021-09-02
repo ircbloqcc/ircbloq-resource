ec062f07cf16046d4fd59d9b9043e55f99435356
ircbloqcc <86342573+ircbloqcc@users.noreply.github.com>
2021-09-02 22:47:42 +0530

    Fix Undefined type err

--- src/server.js
+++ src/server.js
@@ -77,7 +77,7 @@ class IrcBloqResourceServer extends Emitter{
 
         this.emit('ready');
         console.log(`\n----------------------------------------`);
-        console.log(`\x1B[32msocket server listend: http://0.0.0.0:${this._socketPort}\nOpenblock ${this._type} server start successfully\x1B[0m`);
+        console.log(`\x1B[32msocket server listend: http://0.0.0.0:${this._socketPort}\nircBloq resource server start successfully\x1B[0m`);
         console.log(`----------------------------------------\n`);
     }
 }
