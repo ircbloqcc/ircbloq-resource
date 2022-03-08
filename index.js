0736e0b126b2505f5b51c53f92f2e933550a7621
ircbloqcc <86342573+ircbloqcc@users.noreply.github.com>
2022-03-08 16:41:51 +0530

    optimize resource code

--- index.js
+++ index.js
@@ -1,148 +1,161 @@
-const fs = require('fs');
-const copydir = require('copy-dir');
+const fs = require('fs-extra');
+const Emitter = require('events');
 const path = require('path');
 const compareVersions = require('compare-versions');
-const rimraf = require('rimraf');
+const clc = require('cli-color');
+const lockFile = require('proper-lockfile');
 
 const ResourceServer = require('./src/server');
-const GIT = require('./src/git');
+const ResourceUpdater = require('./src/updater');
+const {checkDirHash} = require('./src/calc-dir-hash');
+const {INIT_RESOURCES_STEP} = require('./src/state');
+const getConfigHash = require('./src/get-config-hash');
+const {
+    DIRECTORY_NAME,
+    DEFAULT_USER_DATA_PATH,
+    DEFAULT_LOCALE,
+    RECHECK_INTERVAL
+} = require('./src/config');
 
-/**
- * Configuration the default user data path.
- * @readonly
- */
-const DEFAULT_USER_DATA_PATH = path.join(__dirname, '../.ircbloqData');
+class IrcbloqResourceServer extends Emitter{
+    constructor (userDataPath, initialResourcesPath, locale = DEFAULT_LOCALE) {
+        super();
 
-const DEFAULT_LOCALE = 'en';
-
-class IrcbloqResourceServer {
-    constructor (userDataPath, initialResourcesPath) {
         if (userDataPath) {
-            this._userDataPath = path.join(userDataPath, 'external-resources');
+            this._userDataPath = path.join(userDataPath, DIRECTORY_NAME);
         } else {
-            this._userDataPath = path.join(DEFAULT_USER_DATA_PATH, 'external-resources');
+            this._userDataPath = path.join(DEFAULT_USER_DATA_PATH, DIRECTORY_NAME);
         }
+        this._configPath = path.join(this._userDataPath, 'config.json');
 
-        // path to store initial resources.
+        // The path that store initial resources.
         if (initialResourcesPath) {
             this._resourcesPath = path.join(initialResourcesPath);
         } else {
-            this._resourcesPath = path.join(__dirname, 'external-resources');
+            this._resourcesPath = path.join(__dirname, DIRECTORY_NAME);
         }
 
-        this._configPath = path.join(this._userDataPath, 'config.json');
-
-        this._locale = DEFAULT_LOCALE;
-        this._extensionTransferred = 0;
-
-        this.copyToUserDataPath();
-    }
-
-    copyToUserDataPath () {
-        if (!fs.existsSync(this._configPath)) {
-            console.log(`copy ${this._resourcesPath} to ${this._userDataPath}`);
-            if (!fs.existsSync(this._userDataPath)) {
-                fs.mkdirSync(this._userDataPath, {recursive: true});
-            }
-            copydir.sync(this._resourcesPath, this._userDataPath);
-        }
-    }
-
-    checkUpdate (locale = DEFAULT_LOCALE) {
         this._locale = locale;
-        return new Promise((resolve, reject) => {
-            if (fs.existsSync(this._configPath)) {
 
-                this._config = require(this._configPath); // eslint-disable-line global-require
-
-                if (this._config.repository) {
-                    if (this.git) {
-                        delete this.git;
-                    }
-                    this.git = new GIT(this._config.repository, this._locale);
-
-                    this.git.getLatestReleases()
-                        .then(info => {
-                            if (info.version) {
-                                console.log('info=', info);
-                                const latestVersion = info.version;
-
-                                if (this._config.version) {
-                                    const curentVersion = this._config.version;
-                                    if (compareVersions.compare(latestVersion, curentVersion, '>')) {
-                                        this._latestVersion = latestVersion;
-                                        return resolve({version: latestVersion, describe: info.body});
-                                    }
-                                } else {
-                                    return reject(`Cannot find version tag in: ${this._configPath}`);
-                                }
-                                return resolve();
-                            }
-                            return reject(`Cannot get valid releases from: ${this._config.repository}`);
-                        })
-                        .catch(err => reject(err));
-
-                } else {
-                    return reject(`Cannot find valid repository in: ${this._configPath}`);
-                }
-            } else {
-                return reject(`Cannot find file: ${this._configPath}`);
-            }
-        });
+        this._latestVersion = null;
+        this.updater = null;
     }
 
-    upgrade (callback = null) {
-        return new Promise((resolve, reject) => {
-            if (!this._latestVersion) {
-                return resolve();
-            }
+    checkResources () {
+        if (!fs.existsSync(this._configPath)){
+            return Promise.reject(`Cannot find config file: ${this._configPath}`);
+        }
 
+        const dirHash = getConfigHash(this._configPath);
+
+        // If no hash value in config file, report a warning but don't stop the process.
+        if (!dirHash) {
+            console.warn(clc.yellow(`WARN: no hash value found in ${this._configPath}`));
+            return Promise.resolve();
+        }
+
+        return checkDirHash(this._userDataPath, dirHash);
+    }
+
+    initializeResources (callback = null) {
+        if (callback) {
+            callback({phase: INIT_RESOURCES_STEP.verifying});
+        }
+        fs.ensureDirSync(this._userDataPath);
+
+        const copyResources = () => {
+            console.log(`copy ${this._resourcesPath} to ${this._userDataPath}`);
             if (callback) {
-                callback({phase: 'downloading'});
-            }
-            const downloadPath = path.join(this._userDataPath, '../tmp/download-external-resources');
-
-            if (fs.existsSync(downloadPath)) {
-                rimraf.sync(downloadPath);
-            } else {
-                fs.mkdirSync(downloadPath, {recursive: true});
+                callback({phase: INIT_RESOURCES_STEP.copying});
             }
 
-            this.git.cloneLatestReleases(downloadPath)
+            // copy the initial resources to user data directory
+            return fs.mkdirs(this._userDataPath)
+                .then(() => fs.copy(this._resourcesPath, this._userDataPath))
                 .then(() => {
-                    if (callback) {
-                        callback({phase: 'covering'});
+                    lockFile.unlockSync(this._userDataPath);
+                    return this.checkResources();
+                });
+        };
 
-                        // rm the old data
-                        rimraf.sync(this._userDataPath);
+        const waitUntillInitializeFinish = () => {
+            if (lockFile.checkSync(this._userDataPath)) {
+                setTimeout(() => {
+                    console.log(clc.yellow(`WARN: A resource initialize process is already running, will recheck proccess state after ${RECHECK_INTERVAL} ms`)); // eslint-disable-line max-len
+                    waitUntillInitializeFinish();
+                }, RECHECK_INTERVAL);
+            } else {
+                this.emit('initialize-finish');
+            }
+        };
 
-                        copydir.sync(downloadPath, this._userDataPath,
-                            {
-                                filter: (stat, filepath, filename) => {
-                                    // do not want copy .git directories
-                                    if (stat === 'directory' && filename === '.git') {
-                                        return false;
-                                    }
-                                    return true; // remind to return a true value when file check passed.
-                                }
-                            });
+        if (lockFile.checkSync(this._userDataPath)) {
+            waitUntillInitializeFinish();
+            return new Promise(resolve => {
+                this.on('initialize-finish', () => {
+                    resolve();
+                });
+            });
+        }
 
-                        // write the new version tag to config.json to finitsh upload
-                        const config = Object.assign({}, this._config);
-                        config.version = this._latestVersion;
-                        fs.writeFileSync(this._configPath, JSON.stringify(config));
-                        return resolve();
-                    }
-                })
-                .catch(err => reject(err));
-        });
+        lockFile.lockSync(this._userDataPath);
+        return this.checkResources()
+            .then(() => {
+                lockFile.unlockSync(this._userDataPath);
+            })
+            .catch(e => {
+                console.log(clc.yellow(`WARN: Check resources failed, try to initialize resources: ${e}`));
+                if (fs.existsSync(this._userDataPath)){
+                    return fs.rm(this._userDataPath, {recursive: true, force: true})
+                        .then(() => copyResources());
+                }
+                return copyResources();
+            });
+    }
+
+    checkUpdate (option) {
+        let config;
+
+        try {
+            config = JSON.parse(fs.readFileSync(this._configPath, 'utf8'));
+        } catch (e) {
+            return Promise.reject(e);
+        }
+
+        if (!this.updater) {
+            this.updater = new ResourceUpdater(
+                this._locale === 'CN' && config.updater.cn ? config.updater.cn : config.updater.default,
+                path.dirname(this._userDataPath));
+        }
+
+        return this.updater.checkUpdate(option)
+            .then(info => {
+                info.currentVersion = config.version;
+                if (compareVersions(info.latestVersion, info.currentVersion) > 0) {
+                    info.updateble = true;
+                } else {
+                    info.updateble = false;
+                }
+                this._latestVersion = info.latestVersion;
+                return info;
+            });
+    }
+
+    update (option) {
+        return this.updater.update(this._latestVersion, option);
     }
 
     listen (port = null) {
         const server = new ResourceServer(this._userDataPath);
 
         server.on('error', e => {
-            console.warn(e);
+            this.emit('error', e);
+        });
+        server.on('ready', () => {
+            this.emit('ready');
+        });
+        server.on('port-in-use', () => {
+            this.emit('port-in-use');
         });
 
         server.listen(port);
